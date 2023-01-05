d990dcf01407b8856231c73a5fd8ae6f42d80beb
CortesCode <cacortesl01@gmail.com>
2023-01-05 14:07:46 -0500

    Instalación de el loader de babel y sus plugins. Actualizando archivo de configuración de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+/** @type {import('webpack').Configuration} */
+
 //path es un elemento disponible dentro de node.js
 const path = require("path");
 //Crear un módulo que se va a exportar conteniendo un objeto con la configuración deseada
@@ -12,6 +14,17 @@ module.exports = {
     },
     resolve: {
         //Para identificar con que extensiones va a trabajar
-        extensions: [".js"]
+        extensions: [".js"],    
+    },
+    module:{
+        rules:[
+            {
+                test:/\.m?js$/,//para que utilice los .mjs o sino los .js
+                exclude: /node_modules/,//para que no busque extensiones .js o mjs en node_modules
+                use:{
+                    loader: "babel-loader",//Este es el cargador para usar babel con webpack
+                }
+            }
+        ]
     }
 }
